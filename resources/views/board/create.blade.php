3951bf0c4dae33cf576139d050466cb1f3b4e91e
Harang0705 <t62711691@gmail.com>
2022-09-09 16:24:25 +0900

    cretate page 구현

--- resources/views/board/create.blade.php
+++ resources/views/board/create.blade.php
@@ -1,3 +1,15 @@
 @include('template.header')
-
+<div class="testBoardCreateInner">
+    <div class="testBoardCreateTitle">
+        <ul class="testBoardCreateTitleArea">
+            <li class="testBoardCreateTitleText"><p>제목</p></li>
+            <li class="testBoardCreateTitleInput"><input type="text" name="title" id="testBoardTitle" minlength="5" maxlength="30"></li>
+        </ul>
+    </div>
+    <div class="testBoardCreateContent">
+        <div class="testBoardCreateContentText">
+            <p>내용</p>
+        </div>
+    </div>
+</div>
 @include('template.footer')
