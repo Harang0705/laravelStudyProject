2eaf78c13b12332b91d12ff59dbad1d2ad8d16b4
Harang0705 <t62711691@gmail.com>
2022-09-22 23:05:03 +0900

    route 추가 (board edit, update)

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,6 @@ Route::prefix('board')->group(function () {
     Route::get('/', [boardController::class, 'index'])->name('board.index');
     Route::get('/create', [boardController::class, 'create'])->name('board.create');
     Route::post('/store', [boardController::class, 'store'])->name('board.store');
+    Route::get('/edit/{id}', [boardController::class, 'edit'])->name('board.edit');
+    Route::post('/update', [boardController::class, 'update'])->name('board.update');
 });
