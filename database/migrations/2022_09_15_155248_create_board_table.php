5c2977a73d90b0fdd1136a13b3d54ef0a38cea1e
Harang0705 <t62711691@gmail.com>
2022-09-22 23:01:36 +0900

    board migration file 생성

---
+++ database/migrations/2022_09_15_155248_create_board_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->comment('제목');
+            $table->string('content')->comment('내용');
+            $table->bigInteger('hit')->comment('조회수');
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
